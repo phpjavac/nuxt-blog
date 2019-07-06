c0b5b7614fd9b046f780cca068ba52a70e099f8a
yiziluoying <1060854705@qq.om>
2019-07-06 20:23:50 +0800

    push

---
+++ components/layout/TheHeader.vue
@@ -0,0 +1,177 @@
+<template lang="pug">
+  header.header
+    v-flex(class="header-present" pa-0 wrap)
+      // Mobile nav drawer
+      the-nav-drawer(
+        v-if="false"
+        :pages="pages"
+      )
+
+      // Mobile navbar
+      the-navbar-mobile(
+        v-if="false"
+        :didScroll="didScroll"
+        :pages="pages"
+        :class="didScroll ? 'nav-sticky' : 'nav-transparent'"
+      )
+
+      // Desktop navbar
+      the-navbar-desktop(
+        v-else
+        :didScroll="didScroll"
+        :pages="pages"
+        :class="didScroll ? 'nav-sticky' : 'nav-transparent'"
+      )
+
+    // Background
+    div.header-background
+      // Brand
+      div.header-brand.text-xs-center.secondary-text
+        div.brand-name.display-2
+          span.hidden-text M
+          span.hidden-text r
+          span.hidden-text _
+          span H
+          span i
+          span.hidden-text e
+          span.hidden-text i
+          span.present-text !
+
+        div.brand-motto.subheading
+          span.hidden-text 万
+          span.hidden-text 物
+          span.hidden-text 有
+          span.hidden-text 因
+          span.hidden-text 必
+          span.hidden-text 有
+          span.hidden-text 果
+          span.hidden-text ，
+          span.hidden-text 洗
+          span.hidden-text 洗
+          span.hidden-text 皆
+          span.hidden-text 可
+          span.hidden-text 吃
+</template>
+<script>
+export default {
+  components: {
+    TheNavbarMobile: () => import("./mobile/TheNavbar.vue"),
+    TheNavbarDesktop: () => import("./desktop/TheNavbar.vue"),
+    TheNavDrawer: () => import("@/components/layout/mobile/TheNavDrawer.vue")
+  },
+  props: {
+    didScroll: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+      pages: [
+        {
+          //   path: localePath("index"),
+          icon: "fas fa-home",
+          text: "首页"
+        },
+        {
+          //   path: localePath("tags"),
+          icon: "fas fa-tags",
+          text: "首页"
+        },
+        {
+          //   path: localePath("archives"),
+          icon: "fas fa-archive",
+          text: "首页"
+        },
+        {
+          //   path: localePath("about"),
+          icon: "fas fa-portrait",
+          text: "首页"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+$mb = 120px
+
+.header
+  margin-bottom $mb
+  // Container
+  &-present
+    height 60px
+    >>> .v-icon
+     transition color .3s cubic-bezier(0.25, 0.8, 0.5, 1) !important
+    // Animation
+    .nav-transparent
+      position relative !important
+      padding 15px 0
+      background-color transparent
+      >>> .v-icon
+        color var(--v-secondary-base)
+    .nav-sticky
+      background-color white
+      >>> .v-icon
+        color var(--v-primary-base)
+
+    // Nav
+    .header-nav
+      text-align center
+      transition-property background-color, padding
+      transition-duration .3s
+      transition-timing-function ease-out
+      transition-delay .1s
+      .header-logo
+        margin 0
+      +screen-width-less-than
+        padding 0
+        >>> .v-toolbar__content
+          padding 0
+
+  // Background
+  &-background
+    height 360px
+    position absolute
+    top 0
+    left 0
+    right 0
+    background-image url('/sites/upload/cover-3.jpg')
+    background-repeat no-repeat
+    background-size cover
+    background-position center center
+    &::before
+      content ''
+      width 100%
+      height 100%
+      position absolute
+      top 0
+      left 0
+
+  // Brand
+  &-brand
+    position relative
+    top $mb
+    max-width 360px
+    margin 0 auto
+
+    .brand-name, .brand-motto
+      display flex
+      justify-content center
+
+    .brand-motto{
+      margin-top :$spacer
+    }
+    .hidden-text
+      max-width 0
+      opacity 0
+      transition all .5s ease-in
+
+    &:hover .hidden-text
+      max-width 1em
+      opacity 1
+    &:hover .present-text
+      max-width 0
+      opacity 0
+</style>
--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import * as axios from 'axios'
 
 let options = {}
 // The server-side needs a full url to works
-if (process.server) {
+if (process.env) {
   options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`
 }
 
