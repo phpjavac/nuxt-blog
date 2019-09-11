820d1587fa1757d09b2d7f94e0df75f4e00a96ee
绎紫洛英 <1060854705@qq.com>
2019-09-12 01:21:32 +0800

    build

---
+++ .nuxt/App.js
@@ -0,0 +1,120 @@
+import Vue from 'vue'
+import NuxtLoading from './components/nuxt-loading.vue'
+
+import '..\\node_modules\\vuetify\\dist\\vuetify.css'
+
+import '..\\assets\\style\\stylus\\app.styl'
+
+import _6f6c098b from '..\\layouts\\default.vue'
+
+const layouts = { "_default": _6f6c098b }
+
+export default {
+  head: {"titleTemplate":"%s - yiziluoying-blog","title":"yiziluoying-blog","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"},{"hid":"description","name":"description","content":"My bee&#39;s knees Nuxt.js project"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"},{"rel":"stylesheet","href":"https:\u002F\u002Fcdnjs.cloudflare.com\u002Fajax\u002Flibs\u002Fgithub-markdown-css\u002F3.0.1\u002Fgithub-markdown.css"},{"rel":"stylesheet","href":"https:\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Roboto:300,400,500,700|Material+Icons"},{"rel":"stylesheet","href":"https:\u002F\u002Fuse.fontawesome.com\u002Freleases\u002Fv5.9.0\u002Fcss\u002Fall.css"},{"rel":"stylesheet","type":"text\u002Fcss","href":"\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Roboto:300,400,500,700|Material+Icons"}],"style":[],"script":[]},
+
+  render(h, props) {
+    const loadingEl = h('NuxtLoading', { ref: 'loading' })
+    const layoutEl = h(this.layout || 'nuxt')
+    const templateEl = h('div', {
+      domProps: {
+        id: '__layout'
+      },
+      key: this.layoutName
+    }, [ layoutEl ])
+
+    const transitionEl = h('transition', {
+      props: {
+        name: 'layout',
+        mode: 'out-in'
+      },
+      on: {
+        beforeEnter(el) {
+          // Ensure to trigger scroll event after calling scrollBehavior
+          window.$nuxt.$nextTick(() => {
+            window.$nuxt.$emit('triggerScroll')
+          })
+        }
+      }
+    }, [ templateEl ])
+
+    return h('div', {
+      domProps: {
+        id: '__nuxt'
+      }
+    }, [loadingEl, transitionEl])
+  },
+  data: () => ({
+    isOnline: true,
+    layout: null,
+    layoutName: ''
+  }),
+  beforeCreate() {
+    Vue.util.defineReactive(this, 'nuxt', this.$options.nuxt)
+  },
+  created() {
+    // Add this.$nuxt in child instances
+    Vue.prototype.$nuxt = this
+    // add to window so we can listen when ready
+    if (process.client) {
+      window.$nuxt = this
+      this.refreshOnlineStatus()
+      // Setup the listeners
+      window.addEventListener('online', this.refreshOnlineStatus)
+      window.addEventListener('offline', this.refreshOnlineStatus)
+    }
+    // Add $nuxt.error()
+    this.error = this.nuxt.error
+  },
+
+  mounted() {
+    this.$loading = this.$refs.loading
+  },
+  watch: {
+    'nuxt.err': 'errorChanged'
+  },
+
+  computed: {
+    isOffline() {
+      return !this.isOnline
+    }
+  },
+  methods: {
+    refreshOnlineStatus() {
+      if (process.client) {
+        if (typeof window.navigator.onLine === 'undefined') {
+          // If the browser doesn't support connection status reports
+          // assume that we are online because most apps' only react
+          // when they now that the connection has been interrupted
+          this.isOnline = true
+        } else {
+          this.isOnline = window.navigator.onLine
+        }
+      }
+    },
+
+    errorChanged() {
+      if (this.nuxt.err && this.$loading) {
+        if (this.$loading.fail) this.$loading.fail()
+        if (this.$loading.finish) this.$loading.finish()
+      }
+    },
+
+    setLayout(layout) {
+      if (!layout || !layouts['_' + layout]) {
+        layout = 'default'
+      }
+      this.layoutName = layout
+      this.layout = layouts['_' + layout]
+      return this.layout
+    },
+    loadLayout(layout) {
+      if (!layout || !layouts['_' + layout]) {
+        layout = 'default'
+      }
+      return Promise.resolve(layouts['_' + layout])
+    }
+  },
+  components: {
+    NuxtLoading
+  }
+}
---
+++ .nuxt/vuetify.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+import Vuetify from 'vuetify';
+
+Vue.use(Vuetify, {
+  "theme": {
+    "primary": "#3D4451",
+    "secondary": "#D7D7D7",
+    "accent": "#E8676B"
+  }
+})
